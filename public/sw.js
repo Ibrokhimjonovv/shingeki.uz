5dcf2cc449911474fa78bc1317a0bbd67ea07d37
Ibrokhimjonovv <megabytekekabyte@gmail.com>
2026-07-13 09:56:58 +0500

    .

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11243443
+    "zoneId": 11277817
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
